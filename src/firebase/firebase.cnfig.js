26de97a82160386ff01661a6bcb9ff6729428b13
merazzak <info.merazzak@gmail.com>
2024-12-04 17:54:59 +0600

    first commit

---
+++ src/firebase/firebase.cnfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAM0S35PFozEGzvU9tEqnr8J-5_-XJfSkI",
+  authDomain: "assignment-10-6510b.firebaseapp.com",
+  projectId: "assignment-10-6510b",
+  storageBucket: "assignment-10-6510b.firebasestorage.app",
+  messagingSenderId: "372201131809",
+  appId: "1:372201131809:web:6a6e507c7f427f971af860",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
